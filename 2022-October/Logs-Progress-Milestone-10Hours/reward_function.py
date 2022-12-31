44e6ac835ad4a153e5d32f0e15f4837a72d57f66
Rohan Dugad <rohand2468@gmail.com>
2022-12-31 14:57:31 -0500

    Trained Progress-Milestone for 10 hours

---
+++ 2022-October/Logs-Progress-Milestone-10Hours/reward_function.py
@@ -0,0 +1,23 @@
+class PARAMS:
+    prev_progress = None
+    prev_steps = None
+    last_milestone = 0
+    last_steps = 0
+
+def reward_function(params):
+    if PARAMS.prev_steps is None or params['steps'] < PARAMS.prev_steps:
+        PARAMS.prev_progress = params['progress']
+        PARAMS.last_milestone = 0
+        PARAMS.last_steps = 0
+
+    reward = 0
+    pi = int(params['progress'] // 10)
+    if pi > PARAMS.last_milestone:
+        reward += 200 - (params['steps'] - PARAMS.last_steps)
+        PARAMS.last_milestone = pi
+        PARAMS.last_steps = params['steps']
+
+    PARAMS.prev_progress = params['progress']
+    PARAMS.prev_steps = params['steps']
+    return float(reward)
+
