111ae3487d6753d815e4782fdbcd42793b9744c3
Rohan Dugad <rohand2468@gmail.com>
2023-01-01 18:27:35 -0500

    Trained Milestone-Every2-NoCrash for 6 hours

---
+++ 2022-October/Model-Milestone-Every2-NoCrash.py
@@ -0,0 +1,29 @@
+class PARAMS:
+    prev_progress = None
+    prev_steps = None
+    last_milestone = 0
+    last_steps = 0
+    reward_bucket = 0
+
+def reward_function(params):
+    if PARAMS.prev_steps is None or params['steps'] < PARAMS.prev_steps:
+        PARAMS.prev_progress = params['progress']
+        PARAMS.last_milestone = 0
+        PARAMS.last_steps = 0
+        PARAMS.reward_bucket = 0
+
+    reward = PARAMS.reward_bucket
+    PARAMS.reward_bucket = 0
+    pi = int(params['progress'] // 2)
+    if pi > PARAMS.last_milestone:
+        reward += 30 - (params['steps'] - PARAMS.last_steps)
+        PARAMS.last_milestone = pi
+        PARAMS.last_steps = params['steps']
+
+    if not params['all_wheels_on_track'] or params['is_offtrack'] or params['is_crashed']:
+        PARAMS.reward_bucket = reward
+        reward = 0
+
+    PARAMS.prev_progress = params['progress']
+    PARAMS.prev_steps = params['steps']
+    return float(reward)
